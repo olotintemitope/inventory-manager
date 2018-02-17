df2b0d0d093c513a779544f546c6dae5e22becf2
Temitope Olotin <temitope.olotin@andela.com>
2018-02-17 17:52:59 +0100

    create user repository

---
+++ app/Repository/UserRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Repository\Contract\InventoryInterface;
+
+class UserRepository implements InventoryInterface
+{
+	public function findById($id)
+	{
+
+	}
+
+	public function find($id, $columns)
+	{
+
+	}
+ 
+    public function findBy($field, $value, $columns)
+    {
+
+    }
+
+	pubic function findWhere($field, $value)
+	{
+
+	}
+
+	public function findAll()
+	{
+
+	}
+
+	public function create(array $array)
+	{
+
+	}
+
+	public function update(array $data, $id)
+	{
+		
+	}
+}
