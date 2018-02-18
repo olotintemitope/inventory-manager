b46ce7feeaa05396aaef9a8760a21d7e17940a61
Temitope Olotin <temitope.olotin@andela.com>
2018-02-18 15:55:48 +0100

    include each business to the owner

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -4,9 +4,19 @@ namespace App\Transformers;
 
 use App\Model\User;
 use League\Fractal\TransformerAbstract;
+use App\Transformers\BusinessTransformer;
 
 class UserTransformer extends TransformerAbstract
 {
+	/**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        'businesses'
+    ];
+
     public function transform(User $user)
     {
         return [
@@ -18,10 +28,10 @@ class UserTransformer extends TransformerAbstract
         ];
     }
 
-    // public function includeBusiness(User $user)
-    // {
-    // 	$businesses = $user->businesses;
+    public function includeBusinesses(User $user)
+    {
+    	$businesses = $user->businesses;
 
-    //     return $this->collection($businesses, new BusinessTransformer);
-    // }
+        return $this->collection($businesses, new BusinessTransformer);
+    }
 }
