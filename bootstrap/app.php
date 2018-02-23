8a8fcc60a865c0b03773604a316a87a5ef96885c
Temitope Olotin <temitope.olotin@andela.com>
2018-02-23 16:04:39 +0100

    remove service register

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,7 +41,7 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->register(Dingo\Api\Provider\LaravelServiceProvider::class);
+//$app->register(Dingo\Api\Provider\LaravelServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
